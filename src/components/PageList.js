74180bde273cd326865cd023ce2b37d0d58761c9
Ayush_srivastava <jai62869@gmail.com>
2022-07-20 10:25:24 +0530

    added loading message to the PageList

--- src/components/PageList.js
+++ src/components/PageList.js
@@ -1,12 +1,27 @@
 // after login this page will display
 import React from "react";
 import * as API from '../api';
+import {Link} from 'react-router';
 export default class PageList extends React.Component{
     state = {
+        // when page is loading to display content 
+        loaded:false,
+        pages:{},
         newPageTitle:''
     }
+    componentDidMount() {
+        API.pages.on('value' , ss => this.setState({
+            pages: ss.exportVal() || {},
+            loaded: true
+        }));
+    }
     render (){
+        let items = this.state.loading ? object.keys(this.state.pages).map(id => <li key={id}>
+            <Link to='page' params = { {id:id}}>{this.state.pages[id].title}</Link>
+        </li>):
+            [<li key ='loading'><em> Loading ....</em></li>]
         return<div>
+            <ul> {items} </ul>
             {this.props.user ?
                 <input type = 'text'
                         className = 'u-full-width'
