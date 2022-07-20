bacadf3cf5d2d7f8996576bce71b36b60c4571c2
Ayush_srivastava <jai62869@gmail.com>
2022-07-20 10:47:17 +0530

    adding route of page and importing page component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Router from 'react-router';
 import App from './components/App';
+import Page from './components/Page';
 const {Route} = Router;
 // the way it will render
-var routes = <Route handler={App}></Route>;
+var routes = <Route handler={App}>
+
+    <Route name = 'page' path = '/page/:id' handler = {page} />
+</Route>;
+// adding page route
 Router.run (routes, Router.HistoryLocation, Root =>
     React.render(<Root />, document.getElementById('app')));
